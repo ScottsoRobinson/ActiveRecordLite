407a7d3b36bfcbcb3f50884eb85d8fc59f7c9b58
Scott Robinson <Scott.M.Robinson@gmail.com>
2015-02-06 17:09:38 -0500

    Most of 03_associatable_spec completed

--- active_record_lite/lib/03_associatable.rb
+++ active_record_lite/lib/03_associatable.rb
@@ -10,18 +10,18 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    @class_name.underscore + "s"
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
     defaults = {
-      :class_name => name.capitalize,
+      :class_name => name.to_s.capitalize,
       :foreign_key => "#{name}_id".to_sym,
       :primary_key => :id
     }
@@ -38,8 +38,8 @@ class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
 
     defaults = {
-      :class_name => name.singularize.camelcase,
-      :foreign_key => (self_class_name.underscore + "_id").to_sym,
+      :class_name => name.to_s.singularize.camelcase,
+      :foreign_key => (self_class_name.to_s.underscore + "_id").to_sym,
       :primary_key => :id
     }
     options = defaults.merge(options)
@@ -52,11 +52,27 @@ end
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      foreign = options.foreign_key
+      classname = options.model_class
+      primary = options.primary_key
+      classname.where(primary.to_sym => self.send(foreign)).first
+
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+     options = HasManyOptions.new(name, self.name, options)
+
+    define_method(name) do
+      foreign = options.foreign_key
+      classname = options.model_class
+      primary = options.primary_key
+      classname.where(foreign.to_sym => self.send(primary))
+
+    end
   end
 
   def assoc_options
@@ -65,5 +81,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
